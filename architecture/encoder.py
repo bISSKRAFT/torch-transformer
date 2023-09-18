3fb72d0e4d02d05ccf79f2ec14c9af53b0494d63
crypto <donischle75565@gmail.de>
2023-09-18 17:20:19 +0200

    feat: implement decoder layer

--- architecture/encoder.py
+++ architecture/encoder.py
@@ -1,10 +1,6 @@
 import torch
 import torch.nn as nn
-import copy
-
-def clones(module, N):
-    "Produce N identical layers."
-    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+from helper import clones, LayerNorm, SublayerConnection
 
 class Encoder(nn.Module):
     "Core encoder is a stack of N layers"
@@ -14,36 +10,19 @@ class Encoder(nn.Module):
         self.layers = clones(layer, N)
         self.norm = LayerNorm(layer.size)
 
-class LayerNorm(nn.Module):
-    "Construct a layernorm module (See citation for details)."
-    
-    def __init__(self, features, eps=-1e6):
-        super(LayerNorm, self).__init__()
-        self.a_2 = nn.Parameter(torch.ones(features))
-        self.b_2 = nn.Parameter(torch.zeros(features))
-        self.eps = eps
-
-    def forward(self, x):
-        mean = x.mean(-1, keepdim=True)
-        std = x.std(-1, keepdim=True)
-        return self.a_2 * (x- mean) / (std + self.eps) + self.b_2
-
-
-class SublayerConnection(nn.Module):
-    """
-    A residual connection followed by layer norm.
-    Note for code simplicity the norm is first as opposed to last.
-    """ 
-
-    def __init__(self, size, dropout):
-        super(SublayerConnection, self).__init__()
-        self.norm = LayerNorm(size)
-        self.dropout = nn.Dropout(dropout)
-
-    def forward(self, x, sublayer):
-        "Apply residual connection to any sublayer with the same size."
-        return x + self.dropout(sublayer(self.norm(x)))
     
 class EncoderLayer(nn.Module):
-    "Encoder is made up of self-attn and feed forward (define below)"
-    pass
+    "Encoder is made up of self-attn and feed forward (defined below)"
+    
+    def __init__(self, size, self_attn, feed_forward, dropout):
+        super(EncoderLayer, self).__init__()
+        self.self_attn = self_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 2)
+        self.size = size
+
+    def forward(self, x, mask):
+        x = self.sublayer[0](x,lambda x: 
+                             self.self_attn(x, x, x, mask))
+        
+        return self.sublayer[1](x, self.feed_forward)
