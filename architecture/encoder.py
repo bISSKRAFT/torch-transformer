206cb458e2bc2445727d5a6ecf4d9f6b7a20b830
Leo Donisch <leodonisch@posteo.de>
2023-09-18 07:28:37 +0000

    feat(encoder): implement encoder layer

---
+++ architecture/encoder.py
@@ -0,0 +1,29 @@
+import torch
+import torch.nn as nn
+import copy
+
+def clones(module, N):
+    "Produce N identical layers."
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+
+class Encoder(nn.Module):
+    "Core encoder is a stack of N layers"
+
+    def __init__(self, layer, N):
+        super(Encoder, self).__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+
+class LayerNorm(nn.Module):
+    "Construct a layernorm module (See citation for details)."
+    
+    def __init__(self, features, eps=-1e6):
+        super(LayerNorm, self).__init__()
+        self.a_2 = nn.Parameter(torch.ones(features))
+        self.b_2 = nn.Parameter(torch.zeros(features))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.a_2 * (x- mean) / (std + self.eps) + self.b_2 
